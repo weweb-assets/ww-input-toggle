fe6ff6dc4355e6103ffaf2da42940ea554aa007d
Aurélie VIOLETTE <aurelie.violette@purple-fox.fr>
2021-12-08 15:03:41 +0100

    Fix initial value

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -37,7 +37,7 @@ export default {
     },
     mounted() {
         if (this.content.initialValue !== undefined) {
-            this.checked = !!value;
+            this.checked = !!this.content.initialValue;
         }
     },
     computed: {
