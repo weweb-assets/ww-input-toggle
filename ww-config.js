641f4fc8c60fe7688d0a55f197d163acff31ff7c
Aurélie Violette <orwel.violette@gmail.com>
2024-08-22 16:19:05 +0200

    Disable background property and remove pointerEvent none

--- ww-config.js
+++ ww-config.js
@@ -6,6 +6,9 @@ export default {
         },
         icon: 'toggle',
     },
+    options: {
+        ignoredStyleProperties: ['background'],
+    },
     triggerEvents: [
         { name: 'change', label: { en: 'On change' }, event: { value: '' }, default: true },
         { name: 'initValueChange', label: { en: 'On init value change' }, event: { value: '' } },
