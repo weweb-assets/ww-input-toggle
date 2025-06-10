f73f332ffe75a0a39cc8692b3d20b35816365088
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2025-06-10 16:26:49 +0200

    Fix readonly state handling in wwElement component to allow editing when in editing mode

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -77,6 +77,7 @@ export default {
         },
         isReadonly() {
             /* wwEditor:start */
+            if (this.isEditing) return false;
             if (this.wwEditorState.isSelected) {
                 return this.wwElementState.states.includes('readonly');
             }
